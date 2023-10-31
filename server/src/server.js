cff4b77cebef0eb08fe27dfd331dfb9160a4b059
Thao Nguyen <thaophuong.tracy@gmail.com>
2023-10-31 13:56:59 +0700

    add: MVC structure

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,7 @@
 import cors from 'cors'
 import express from 'express'
 import dotenv from 'dotenv'
-
-import pool from '../config/db.js'
+import testRouter from './routes/test.router.js'
 
 dotenv.config()
 const app = express()
@@ -13,19 +12,7 @@ app.use(express.urlencoded({ extended: true }))
 
 const port = process.env.PORT || 6969
 
-async function getAllTests(req, res) {
-  try {
-    const allTests = await pool.query('SELECT * FROM test')
-    return res.status(200).json(allTests.rows)
-  } catch (error) {
-    console.error(error.message)
-    return res.status(400).json({ message: error.message })
-  }
-}
-
-const router = express.Router()
-router.get('/', getAllTests)
-app.use('/test', router)
+app.use('/test', testRouter)
 
 app.listen(port, () => {
   console.log('Backend is running on the port: ' + port)
