bdc2141a67a43c3df5d42b602d1429e51be33473
Thao Nguyen <thaophuong.tracy@gmail.com>
2023-11-04 21:22:59 +0700

    add: basic API for tag

--- server/src/server.js
+++ server/src/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 
 import testRouter from './routes/test.router.js'
 import categoryRouter from './routes/category.router.js'
+import tagRouter from './routes/tag.router.js'
 
 dotenv.config()
 const app = express()
@@ -16,6 +17,7 @@ const port = process.env.PORT || 6969
 
 app.use('/test', testRouter)
 app.use('/category', categoryRouter)
+app.use('/tag', tagRouter)
 
 app.listen(port, () => {
   console.log('Backend is running on the port: ' + port)
