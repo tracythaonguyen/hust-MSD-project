54d350f9185829c73b37308eeae735d0d6d3ede4
linhpham39 <linhshark02@gmail.com>
2023-11-07 02:09:45 +0700

    create footer and complete landing page

--- client/src/pages/landing/Landing.jsx
+++ client/src/pages/landing/Landing.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './landing.css'
 import Header from '../../assets/images/Header-landing.png'
-import logo from '../../assets/images/logo.svg'
+import logoIcon from '../../assets/images/logo.svg'
+import playIcon from '../../assets/images/play_icon.png'
+import Footer from '../../components/Footer';
 
 
 
@@ -11,19 +13,38 @@ export const Landing = () => {
             <div className="landingHeader">
                 <img src={Header} className='bg'></img>
                 <div className="header-left">
-                    <img src={logo} className='logo'></img>
+                    <img src={logoIcon} className='logo'></img>
                 </div>
                 <div className="header-right">
-                        <button className='login'>Login</button>
-                        <button className='register'>Register</button>
+                    <button className='login'>Login</button>
+                    <button className='register'>Register</button>
+                </div>
+
+                <div className="header-body">
+                    <div className="title">
+                        <span className='title-color' style={{ color: '#F48C06', marginRight: '10px' }}>Studying</span>
+                        Online is now
+                        <br></br> much easier
                     </div>
-            </div>
-            <div className="landingContainer">
-                <div className="landing-top">
-                    
+                    <div className="desc">
+                        TOTC is an interesting platform that will teach<br></br> you in more an interactive way
+                    </div>
+                    <div className="header-button">
+                        <button className="join-for-free">
+                            Join for free
+                        </button>
+
+                        <div className="watch-work">
+                            <img src={playIcon}></img>
+                            <div className="watch-work_text">
+                                Watch how it works
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <h1>Footer</h1>
+            
+            <Footer></Footer>
         </div>
     )
 }
