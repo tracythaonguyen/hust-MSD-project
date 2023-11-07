e64bce65cb13ec423b44aaa1e204fb8f7e69cd31
Thao Nguyen <thaophuong.tracy@gmail.com>
2023-11-07 17:50:44 +0700

    add: basic API for account

---
+++ server/src/routes/account.router.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import accountController from '../controllers/account.controller.js'
+
+const router = express.Router()
+
+// get all accounts
+router.get('/', accountController.getAllAccounts)
+
+// search account by username
+router.get('/search', accountController.searchAccountByUserName)
+
+// delete an account by id
+router.delete('/:id', accountController.deleteAccount)
+
+// update account username
+router.put('/change-name/:id', accountController.updateUserName)
+
+// create account
+router.post('/create', accountController.createAccount)
+
+export default router
