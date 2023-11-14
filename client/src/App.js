30555003adf054451e85a535909ed99f11acd089
linhpham39 <linhshark02@gmail.com>
2023-11-14 15:13:48 +0700

    create header

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Login from './pages/login/Login'
 import Register from './pages/login/Register'
 import HomePage from './pages/HomePage'
 import Landing from './pages/landing/Landing'
+import UserPage from './pages/user/UserPage'
 import './App.css'
 
 export default function App() {
@@ -16,6 +17,7 @@ export default function App() {
                     <Route path="/login" component={ Login } />
                     <Route path="/register" component={ Register } />
                     <Route path="/home" component={ HomePage } />
+                    <Route path="/user" component={ UserPage } />
                 </Switch>
         
             </div>
