dacb1bd111df8c24da2612dfdc67da882d37be8a
Thao Nguyen <thaophuong.tracy@gmail.com>
2023-12-09 15:23:49 +0700

    cmt account due to bug

--- server/src/routes/account.router.js
+++ server/src/routes/account.router.js
@@ -1,26 +1,25 @@
-import express from 'express'
-import accountController from '../controllers/account.controller.js'
+// import express from 'express'
+// import accountController from '../controllers/account.controller.js'
 
-const router = express.Router()
-//login
-router.post('/login', accountController.login)
-// //register
-// router.post('/register', accountController.createAccount)
+// const router = express.Router()
+// //login
+// router.post('/login', accountController.login)
+// // //register
+// // router.post('/register', accountController.createAccount)
 
+// // get all accounts
+// router.get('/', accountController.getAllAccounts)
 
-// get all accounts
-router.get('/', accountController.getAllAccounts)
+// // search account by username
+// router.get('/search', accountController.searchAccountByUserName)
 
-// search account by username
-router.get('/search', accountController.searchAccountByUserName)
+// // delete an account by id
+// router.delete('/:id', accountController.deleteAccount)
 
-// delete an account by id
-router.delete('/:id', accountController.deleteAccount)
+// // update account username
+// router.put('/change-name/:id', accountController.updateUserName)
 
-// update account username
-router.put('/change-name/:id', accountController.updateUserName)
+// // create account
+// router.post('/create', accountController.createAccount)
 
-// create account
-router.post('/create', accountController.createAccount)
-
-export default router
+// export default router
