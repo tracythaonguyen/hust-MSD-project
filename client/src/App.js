b585ad08ea951ba7fc89679e3799ad11c189f636
Dat Dang <dangdatcao2002@gmail.com>
2023-12-28 23:03:51 +0700

    history page design

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,49 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route, RouteProps, Redirect} from 'react-router-dom'
-import Login from './pages/login/Login'
-import Register from './pages/login/Register'
-import HomePage from './pages/HomePage.jsx'
-import Landing from './pages/landing/Landing'
-import UserPage from './pages/user/UserPage'
-import VideoPage from './pages/video/VideoPage.jsx'
-import './App.css'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  RouteProps,
+  Redirect,
+} from "react-router-dom";
+import Login from "./pages/login/Login";
+import Register from "./pages/login/Register";
+import HomePage from "./pages/HomePage.jsx";
+import Landing from "./pages/landing/Landing";
+import UserPage from "./pages/user/UserPage";
+import VideoPage from "./pages/video/VideoPage.jsx";
+import "./App.css";
 import TopicPage from "./pages/topic/TopicPage";
+import historyPage from "./pages/history/historyPage.jsx";
 // test Ci/cd
 export default function App() {
-    const isLogin = localStorage.getItem('token');
-    const ProtectedRoute = ({ component: Component, path }) => {
-        if (!isLogin) {
-          return <Redirect to="/" />;
-        }
-        return <Route component={Component} path={path} />
-      };
-      const ProtectedRoute2 = ({ component: Component, path }) => {
-        if (isLogin) {
-          return <Redirect to="/home" />;
-        }
-        return <Route component={Component} path={path} />
-      };
-    return (
-        <Router>
-            <div>
-                <Switch>
-                    <ProtectedRoute2 exact path="/" component={ Landing } />
-                    <ProtectedRoute2 path="/login" component={ Login } />
-                    <ProtectedRoute2 path="/register" component={ Register } />
-                    <ProtectedRoute path="/home" component={ HomePage } />
-                    <ProtectedRoute path="/user" component={ UserPage } />
-                    <ProtectedRoute path="/topic" component={ TopicPage } />
-                    <Route path="/video" component={ VideoPage } />
-                </Switch>
-            </div>
-        </Router>
-    )
+  const isLogin = localStorage.getItem("token");
+  const ProtectedRoute = ({ component: Component, path }) => {
+    if (!isLogin) {
+      return <Redirect to="/" />;
+    }
+    return <Route component={Component} path={path} />;
+  };
+  const ProtectedRoute2 = ({ component: Component, path }) => {
+    if (isLogin) {
+      return <Redirect to="/home" />;
+    }
+    return <Route component={Component} path={path} />;
+  };
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <ProtectedRoute2 exact path="/" component={Landing} />
+          <ProtectedRoute2 path="/login" component={Login} />
+          <ProtectedRoute2 path="/register" component={Register} />
+          <ProtectedRoute path="/home" component={HomePage} />
+          <ProtectedRoute path="/user" component={UserPage} />
+          <ProtectedRoute path="/topic" component={TopicPage} />
+          <ProtectedRoute path="/history" component={historyPage} />
+          <Route path="/video" component={VideoPage} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
-
-
-
