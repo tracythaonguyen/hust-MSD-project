127bd5a9bba22d14e2a2bf9fb0e5b5c6ad2fc578
Hà Huy Hoàng <101927338+3hJalter@users.noreply.github.com>
2024-01-02 01:13:07 +0700

    +ADD: Init Search By Tag

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -48,32 +48,36 @@ export default function HomePage() {
         }
     };
 
-    //#region DEV: HOANG
+    //#region NEW FUNCTIONS -> DEV: HOANG
     const [filterData, setFilterData] = useState("");
     //#region -> Searching Functionalities
     // search function with param is input text from search-term className
     const search = (searchTerm) => {
         console.log("Search Term: ", searchTerm);
         setFilterData(searchTerm);
-        if (searchTerm !== "") {
-            const newVideoList = videos.filter((video) => {
-                return Object.values(video)
-                    .join(" ")
-                    .toLowerCase()
-                    .includes(searchTerm.toLowerCase());
-            });
-            // debug
-            console.log("Search Results: ", newVideoList);
-            // Navigate to TopicPage with searchResults
-            history.push({
-                pathname: '/videoList',
-                state: {initVideoListData: newVideoList},
-            });
+        if (searchByTag) {
+            // TODO: Search by tag
         } else {
-            history.push({
-                pathname: '/videoList',
-                state: {initVideoListData: videos},
-            });
+            if (searchTerm !== "") {
+                const newVideoList = videos.filter((video) => {
+                    return Object.values(video)
+                        .join(" ")
+                        .toLowerCase()
+                        .includes(searchTerm.toLowerCase());
+                });
+                // debug
+                console.log("Search Results: ", newVideoList);
+                // Navigate to TopicPage with searchResults
+                history.push({
+                    pathname: '/videoList',
+                    state: {initVideoListData: newVideoList},
+                });
+            } else {
+                history.push({
+                    pathname: '/videoList',
+                    state: {initVideoListData: videos},
+                });
+            }
         }
     }
     //#endregion
@@ -143,6 +147,20 @@ export default function HomePage() {
             state: {initVideoListData: newVideoList},
         });
     }
+    //#region -> Search by tag Functionalities
+    // button to select search by tag or by name
+    const [searchByTag, setSearchByTag] = useState(false);
+    const searchByTagButton = () => {
+        console.log("Search by tag");
+        setSearchByTag(!searchByTag); // Toggle dropdown visibility
+        // change the text of button
+        if (searchByTag) {
+            document.getElementById("search-option-button").innerHTML = "Search by tag";
+        } else {
+            document.getElementById("search-option-button").innerHTML = "Search by name";
+        }
+    }
+
     //#endregion
 
     return (
