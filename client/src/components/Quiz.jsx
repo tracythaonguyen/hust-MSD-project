476377c359666e860fc44ff49bd891efecc2c09f
mnhhuyen <mnhhuyen.26@gmail.com>
2024-01-02 12:44:20 +0700

    update

--- client/src/components/Quiz.jsx
+++ client/src/components/Quiz.jsx
@@ -76,6 +76,26 @@ const FillInTheBlankQuiz = ({ transcript, video_id, track_id }) => {
     }
   };
 
+  var animateButton = function (e) {
+
+    e.preventDefault();
+    //reset animation
+    e.target.classList.remove('animate');
+
+    e.target.classList.add('animate');
+
+    e.target.classList.add('animate');
+    setTimeout(function () {
+      e.target.classList.remove('animate');
+    }, 6000);
+  };
+
+  var classname = document.getElementsByClassName("button");
+
+  for (var i = 0; i < classname.length; i++) {
+    classname[i].addEventListener('click', animateButton, false);
+  }
+
   return (
     <div>
       <p className="question">{renderTranscriptWithBlanks()}</p>
